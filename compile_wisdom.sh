c1c157a155ab4aadcb0c690602369a95cfe4c23b
Lari Natri <larva@aavalla.net>
2017-01-23 02:35:24 +0200

    Add deploying of compiled PDF for one specific user. Sorry for the ugliness.

--- compile_wisdom.sh
+++ compile_wisdom.sh
@@ -7,19 +7,26 @@
 SONGIDXPROG="ext_packages/bin/songidx"
 
 die() {
-  echo "$1" >&2
-  exit 1
+  echo "$2" >&2
+  exit $1
 }
 
 which "pdflatex" >"/dev/null" || die 1 "pdflatex program not found! Aborting."
 
-pdflatex -interaction=nonstopmode wisdom-songbook.tex # first run
+pdflatex -interaction=nonstopmode wisdom-songbook.tex || die $? "Compilation error!" # first run 
 
 if [ -x "$SONGIDXPROG" ]; then
   "$SONGIDXPROG" idx_wisdom-sb_title.sxd idx_wisdom-sb_title.sbx
   "$SONGIDXPROG" idx_wisdom-sb_auth.sxd idx_wisdom-sb_auth.sbx
   # bin/songidx idx_wisdom-sb_scrip.sxd idx_wisdom-sb_scrip.sbx
   pdflatex -interaction=nonstopmode wisdom-songbook.tex # second run
+  ecode=$?
+  # Copy compiled PDF to a specific directory if user is larva. Sorry for this. :)
+  if [ $ecode -eq 0 ] && [ "$USER" = "larva" ] && [ -d "/home/larva/Cloud/ownCloud/wisdom" ]; then
+    cp wisdom-songbook.pdf "/home/larva/Cloud/ownCloud/wisdom/"
+    if [ $? -eq 0 ]; then echo "Compiled PDF copied to 'the cloud'"; fi
+  fi
+  exit $ecode
 else
   echo "$SONGIDXPROG not found. Indexes not created!"
 fi
