unilaiva/unilaiva-songbook

commit be1e7d07cb51f0dbcedb55b37d77fbd820354caa

Minor cleaning

compile_wisdom.sh
@@ -64,7 +64,7 @@ else
 fi
 
 cd "$initial_dir" # get out of $TEMP_DIRNAME
-cp $TEMP_DIRNAME/*.pdf ./ # copy the PDFs to root dir
+cp "$TEMP_DIRNAME/*.pdf" "./" # copy the PDFs to root dir
 
 echo ""
 
@@ -72,9 +72,7 @@ echo ""
 if [ -d "./deploy" ]; then
   cp "wisdom-songbook.pdf" "./deploy/" && echo "Compiled PDF copied to ./deploy/"
   if [ "$printouts_created" = "yes" ]; then
-    cp "printout_wisdom_A5_on_A4_doublesided_folded.pdf" "./deploy/" \
-      && cp "printout_wisdom_A5_on_A4_sidebyside_simple.pdf" "./deploy/" \
-      && echo "Extra printouts copied to ./deploy/"
+    cp "printout*.pdf" "./deploy/" && echo "Extra printouts copied to ./deploy/"
   fi
 else
   echo "PDF not deployed"

commit b1ccb6c67e1afad51fe3005e1402f752259a947e

Fix copy problem introduced by be1e7d07

compile_wisdom.sh
@@ -64,7 +64,7 @@ else
 fi
 
 cd "$initial_dir" # get out of $TEMP_DIRNAME
-cp "$TEMP_DIRNAME/*.pdf" "./" # copy the PDFs to root dir
+cp "$TEMP_DIRNAME/"*.pdf "./" || die $? "Error copying result files from temporary directory! Aborted."
 
 echo ""
 
@@ -72,7 +72,7 @@ echo ""
 if [ -d "./deploy" ]; then
   cp "wisdom-songbook.pdf" "./deploy/" && echo "Compiled PDF copied to ./deploy/"
   if [ "$printouts_created" = "yes" ]; then
-    cp "printout*.pdf" "./deploy/" && echo "Extra printouts copied to ./deploy/"
+    cp printout*.pdf "./deploy/" && echo "Extra printouts copied to ./deploy/"
   fi
 else
   echo "PDF not deployed"