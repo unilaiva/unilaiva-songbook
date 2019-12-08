70759978875611ca1d308925d3124ea047352e15
Lari Natri <larva@aavalla.net>
2019-12-08 23:46:10 +0200

    Add support for compiling selections. Also other small improvements to the compile script.

--- compile_unilaiva-songbook.sh
+++ compile_unilaiva-songbook.sh
@@ -11,7 +11,7 @@
 # for further information about options, or see function print_usage_and_exit
 # below.
 #
-# Required binaries in PATH: lilypond-book, pdflatex, texlua
+# Required binaries in PATH: lilypond-book, pdflatex, texlua, awk
 # Optional binary in PATH: context (will be used to create printout versions)
 #
 
@@ -20,6 +20,7 @@ MAIN_FILENAME_BASE="unilaiva-songbook" # filename base for the main document (wi
 PART1_FILENAME_BASE="unilaiva-songbook_part1" # filename base for the 2-part document's part 1 (without .tex suffix)
 PART2_FILENAME_BASE="unilaiva-songbook_part2" # filename base for the 2-part document's part 2 (without .tex suffix)
 TEMP_DIRNAME="temp" # just the name of a subdirectory, not an absolute path
+SELECTION_FNAME_PREFIX="ul-selection"
 SONG_IDX_SCRIPT="ext_packages/songs/songidx.lua"
 # The following is the locale used in creating the indexes, thus affecting the
 # sort order. Finnish (UTF8) is the default. Note that the locale used must be
@@ -27,8 +28,9 @@ SONG_IDX_SCRIPT="ext_packages/songs/songidx.lua"
 # "locale -a".
 SORT_LOCALE="fi_FI.utf8" # Recommended default: fi_FI.utf8
 
-initial_dir="$PWD"
-error_occurred_filename="${initial_dir}/${TEMP_DIRNAME}/compilation_error_occurred"
+INITIAL_DIR="$PWD" # Store the initial directory
+
+ERROR_OCCURRED_FILE="${INITIAL_DIR}/${TEMP_DIRNAME}/compilation_error_occurred"
 
 # Function: print the program usage informationand exit.
 print_usage_and_exit() {
@@ -37,21 +39,26 @@ print_usage_and_exit() {
   echo ""
   echo "Options:"
   echo ""
-  echo "  --no-partial   : do not compile partial books, only the main document"
-  echo "  --no-printouts : do not create extra printout PDFs"
-  echo "  --no-deploy    : do not copy PDF files to ./deploy/"
-  echo "  --parallel     : compile documents simultaneously"
-  echo "  -d             : use for quick development build of the main document;"
-  echo "                 : equals to --no-partial --no-printouts --no-deploy"
-  echo "  --help         : print this usage information"
+  echo "  --no-partial    : do not compile partial books, only the main document"
+  echo "  --no-printouts  : do not create extra printout PDFs"
+  echo "  --no-selections : do not create selection booklets"
+  echo "  --no-deploy     : do not copy PDF files to ./deploy/"
+  echo "  --parallel      : compile documents simultaneously"
+  echo "  -d              : use for quick development build of the main document;"
+  echo "                  : equals to --no-partial --no-printouts --no-selections"
+  echo "                    --no-deploy"
+  echo "  --help          : print this usage information"
   echo ""
   echo "In addition to the full songbook, also two-booklet version is created,"
   echo "with parts 1 and 2 in separate PDFs. This is not done, if --no-partial"
   echo "option is present."
   echo ""
-  echo "Special versions for printing (printout_unilaiva-songbook_*.pdf) are"
-  echo "created, if 'context' binary is available and --no-printouts option"
-  echo "is not given."
+  echo "Also selection booklets, with specific songs only, specified in files"
+  echo "named ul-selection_*.pdf are compiled, unless --no-selections option"
+  echo "is present."
+  echo ""
+  echo "Special versions for printing (printout_*.pdf) are created, if 'context'"
+  echo "binary is available and --no-printouts option is not given."
   echo ""
   echo "If --no-deploy argument is not present, the resulting PDF files will"
   echo "also be copied to ./deploy/ directory (if it exists)."
@@ -62,24 +69,24 @@ print_usage_and_exit() {
 # Function: exit the program with error code and message.
 # Usage: die <errorcode> <message>
 die() {
-  # Only print errors, if file ${error_occurred_filename} does NOT exist.
+  # Only print errors, if file ${ERROR_OCCURRED_FILE} does NOT exist.
   # If it exists, it means that error processing is already underway,
   # and this is only a child process that has been killed.
-  if [ ! -f "${error_occurred_filename}" ]; then
+  if [ ! -f "${ERROR_OCCURRED_FILE}" ]; then
     # Create the file signifying (for child processes) that we are already
     # dying:
-    echo "Error occurred while compiling: $2 (code: $1)" >"${error_occurred_filename}"
+    echo "Error occurred while compiling: $2 (code: $1)" >"${ERROR_OCCURRED_FILE}"
     echo ""
     echo "ERROR:   $2" >&2
-    cd "${initial_dir}"
+    cd "${INITIAL_DIR}"
     pkill_available="false"
     which "pkill" >"/dev/null" && pkill_available="true"
-    for pid in "${pids[@]}"
-    do
+    for pid in "${pids[@]}"; do # Loop through the main sub processes
+      # If we have 'pkill', kill children of the main sub process first:
       if [ "${pkill_available}" = "true" ]; then
         [ ${pid} != 0 ] && pkill --parent ${pid} -9 >/dev/null 2>&1
       fi
-      # Kill compilation processes:
+      # Kill the main sub process:
       [ ${pid} != 0 ] && kill -9 ${pid} >/dev/null 2>&1
     done
   fi
@@ -92,10 +99,10 @@ compile_document() {
 
   # Usage: die_log <errorcode> <message> <logfile>
   die_log() {
-    # Only print errors, if file ${error_occurred_filename} does NOT exist.
+    # Only print errors, if file ${ERROR_OCCURRED_FILE} does NOT exist.
     # If it exists, it means that error processing is already underway,
     # and this is only a child process that has been killed.
-    if [ ! -f "${error_occurred_filename}" ]; then
+    if [ ! -f "${ERROR_OCCURRED_FILE}" ]; then
       echo "ERROR    [${document_basename}]: $2"
       echo ""
       echo "Displaying log file for ${document_basename}.tex: $3"
@@ -149,13 +156,16 @@ compile_document() {
   # First run of pdflatex:
   pdflatex -interaction=nonstopmode "${document_basename}.tex" 1>"out-2_pdflatex.log" 2>&1 || die_log $? "Compilation error running pdflatex! Aborted." "out-2_pdflatex.log"
 
-  echo "EXEC     [${document_basename}]: texlua (create indices)"
+  # Only create indices, if not compiling a selection booklet (bashism):
+  if [[ ${document_basename} != ${SELECTION_FNAME_PREFIX}* ]]; then
+    echo "EXEC     [${document_basename}]: texlua (create indices)"
 
-  # Create indices:
-  texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} "idx_${document_basename}_title.sxd" "idx_${document_basename}_title.sbx" 1>"out-3_titleidx.log" 2>&1 || die_log $? "Error creating song title indices! Aborted." "out-3_titleidx.log"
-  # Author index creation is commented out, as it is not used (now):
-  # texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} idx_${document_basename}_auth.sxd idx_${document_basename}_auth.sbx 1>"out-4_authidx.log" 2>&1 || die_log $? "Error creating author indices! Aborted." "out-4_authidx.log"
-  texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} -b "tags.can" "idx_${document_basename}_tag.sxd" "idx_${document_basename}_tag.sbx" 1>"out-5_tagidx.log" 2>&1 || die_log $? "Error creating tag (scripture) indices! Aborted." "out-5_tagidx.log"
+    # Create indices:
+    texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} "idx_${document_basename}_title.sxd" "idx_${document_basename}_title.sbx" 1>"out-3_titleidx.log" 2>&1 || die_log $? "Error creating song title indices! Aborted." "out-3_titleidx.log"
+    # Author index creation is commented out, as it is not used (now):
+    # texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} idx_${document_basename}_auth.sxd idx_${document_basename}_auth.sbx 1>"out-4_authidx.log" 2>&1 || die_log $? "Error creating author indices! Aborted." "out-4_authidx.log"
+    texlua "${SONG_IDX_SCRIPT}" -l ${SORT_LOCALE} -b "tags.can" "idx_${document_basename}_tag.sxd" "idx_${document_basename}_tag.sbx" 1>"out-5_tagidx.log" 2>&1 || die_log $? "Error creating tag (scripture) indices! Aborted." "out-5_tagidx.log"
+  fi
 
   echo "EXEC     [${document_basename}]: pdflatex (2nd run)"
 
@@ -206,7 +216,7 @@ compile_document() {
   rm tmp*.out tmp*.sxc 2>"/dev/null"
 
   # Get out of ${temp_dirname_twolevels}:
-  cd "${initial_dir}" || die $? "Cannot return to the main directory."
+  cd "${INITIAL_DIR}" || die $? "Cannot return to the main directory."
 
   # If "--no-deploy" is not given as an argument and the subdirectory 'deploy' exists, copy the
   # final PDFs there also. (The directory is meant to be automatically synced to the server by
@@ -231,6 +241,7 @@ compile_document() {
 deployfinal="true"
 createprintouts="true"
 partialbooks="true"
+selections="true"
 parallel="false"
 
 # Test program arguments:
@@ -245,6 +256,9 @@ while [ $# -gt 0 ]; do
     "--no-partial")
       partialbooks="false"
       shift;;
+    "--no-selections")
+      selections="false"
+      shift;;
     "--parallel")
       parallel="true"
       shift;;
@@ -252,6 +266,7 @@ while [ $# -gt 0 ]; do
       deployfinal="false"
       createprintouts="false"
       partialbooks="false"
+      selections="false"
       shift;;
     *) # for everything else
       print_usage_and_exit
@@ -268,31 +283,51 @@ which "awk" >"/dev/null" || die 1 "'awk' binary not found in path! Aborted."
 # Create the 1st level temporary directory in case it doesn't exist.
 mkdir "$TEMP_DIRNAME" 2>"/dev/null"
 [ -d "./$TEMP_DIRNAME" ] || die 1 "Could not create temporary directory $TEMP_DIRNAME. Aborted."
-rm "${error_occurred_filename}" >/dev/null 2>&1
-
+# Remove the file signifying the last compilation resulted in error,
+# if it exists:
+rm "${ERROR_OCCURRED_FILE}" >/dev/null 2>&1
 
 trap 'die 130 Interrupted.' INT TERM # trap interruptions
 
+# Insert main documents to be compiled to 'docs' array:
+doc_count=0
+docs[doc_count]="${MAIN_FILENAME_BASE}" ; ((doc_count++))
+if [ ${partialbooks} = "true" ]; then  # add partial books
+  docs[doc_count]="${PART1_FILENAME_BASE}" ; ((doc_count++))
+  docs[doc_count]="${PART2_FILENAME_BASE}" ; ((doc_count++))
+fi
+if [ ${selections} = "true" ]; then  # add selecion booklets
+  i=0
+  for f in ${SELECTION_FNAME_PREFIX}*.tex
+  do
+    if [ -f "${f}" ]; then  # if normal file
+      docs[doc_count]="${f%.tex}" ; ((doc_count++))
+    fi
+  done
+fi
+
+parallel_text="sequentially"
+[ ${parallel} = "true" ] && parallel_text="in parallel"
+
 echo ""
-echo "Compiling Unilaiva songbook..."
+echo "Compiling Unilaiva songbook (${doc_count} documents ${parallel_text})..."
 echo ""
 
-# Compile the documents (defined at the top of this file):
-pidcounter=0
-compile_document "${MAIN_FILENAME_BASE}" &
-pids[pidcounter]=$!
-[ ${parallel} = "false" ] && wait && pids[pidcounter]=0 && echo ""
-if [ ${partialbooks} = "true" ]; then
-  ((pidcounter++))
-  compile_document "${PART1_FILENAME_BASE}" &
-  pids[pidcounter]=$!
-  [ ${parallel} = "false" ] && wait && pids[pidcounter]=0 && echo ""
-  ((pidcounter++))
-  compile_document "${PART2_FILENAME_BASE}" &
-  pids[pidcounter]=$!
-  [ ${parallel} = "false" ] && wait && pids[pidcounter]=0 && echo ""
-fi
-wait # wait for sub processes to end
+# Compile the documents in the 'docs' array:
+pid_count=0
+pids[0]=0  # 'pids' array will contain PIDs of sub processes
+for doc in "${docs[@]}"; do
+  compile_document "${doc}" &
+  pids[pid_count]=$!
+  if [ ${parallel} = "true" ]; then
+    ((pid_count++))
+  else
+    wait  # wait for the last compile_document to finish
+    pids[pid_count]=0  # reset PID
+  fi
+done
+
+wait # wait for all sub processes to end
 
 echo ""
 echo "Done."
